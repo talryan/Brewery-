20065b25956aa6e58bda9f4e159331c22267d85b
talryan <nataliamryan@gmail.com>
2020-11-13 10:05:32 -0800

    Refactoring code

--- lib/tap-in/brewery.rb
+++ lib/tap-in/brewery.rb
@@ -2,6 +2,7 @@ class Brewery
 
     attr_accessor :name, :city, :brewery_type, :website_url
     @@all = []
+    @@save_brewery
     def initialize(name, city, brewery_type, website_url)
       
         @name = name
@@ -15,9 +16,9 @@ class Brewery
         @@all
     end
 
-    def self.display_list_of_breweries
-        Brewery.all.each_with_index do |brewery, index|
-           puts "#{index+1}. #{brewery.name}"
+    def self.list_of_breweries
+        Brewery.all.each_with_index do |brewery, i, city|
+           puts "#{i+1}. #{brewery.name} - #{brewery.city}"
         end
     end
 
@@ -28,8 +29,18 @@ class Brewery
         puts "\nCity: " + brewery.city
         puts "\nType: " + brewery.brewery_type
         puts "\nWebsite URL: " + brewery.website_url
+
+
      
     end
+
+    # def self.save_brewery
+    #     puts "Would you like to bookmark this brewery to your travel plans?"
+    #     if user_input == "yes"
+    #         puts "This has been bookmarked."
+    #     else user_input == "no"
+    #         puts "No "
+    # end
 
 
 end
--- lib/tap-in/cli.rb
+++ lib/tap-in/cli.rb
@@ -1,6 +1,7 @@
 class Cli
-# @@array_states = ["Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut","Delaware", "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon" "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia","Wyoming"]
-    def start
+      @@bookmark =[]
+    
+      def start
 
             puts " ########    ###    ########    #### ##    ## "
             puts "   ##      ## ##   ##     ##     ##  ###   ## "
@@ -10,87 +11,67 @@ class Cli
             puts "   ##    ##     ## ##            ##  ##   ### "
             puts "   ##    ##     ## ##           #### ##    ## "
 
-            puts "Your California roadtrip brewery locator"
-
-
+            puts "Your California roadtrip brewery tour!"
             Api.fetch_brewery
-            title_screen
-
-    end
-        
-    
-
-    
-
-      def title_screen  
+            first_selection
+      end
+      
+      def first_selection
             puts "Would you like to tap in to our list of reccommended breweries?" 
-            puts "Enter 'yes' if you would like to proceed."      
-            user_input = gets.strip.downcase
+            puts "Enter 'yes' if you would like to proceed."
+            main_menu
+      end
+
+      def recursion
+            puts "Would you like to check out another brewery?"
+            main_menu
+      end
+
+      def main_menu     
+            user_input = gets.chomp.downcase
             if user_input == "yes"
-                  puts " "
                   puts " "
                   puts " "
                   puts "Take a look at this un-beer-ieveable selection!"
                   puts " "
                   sleep(2)
-                  Brewery.display_list_of_breweries
+                  Brewery.list_of_breweries
                   puts " "
                   sleep(1)
                   puts "Please select a brewery you'd like to know more about! "
                   puts "Enter a number that corresponds to the brewery name."
                   self.brewery_select
-             
-                  title_screen
+                  sleep(2)
+                  recursion
+                  # puts "Here are the breweries you viewed!"
+                  # puts @@bookmark
             elsif user_input == "no"
                   puts "Hmm... Looks like you are not interested in looking at any breweries."
                   sleep(2)
                   puts "Thank brew very much! Have a hoppy day!"
-
             else user_input != "yes" || user_input != "no"
+                  puts " "
+                  puts " "
                   puts "Not a valid input."
                   puts "Try again."
                   puts " "
                   puts " "
-                  title_screen
-            # else user_input == "no"
-            #       puts "Hmm... Looks like you are not interested in looking at any breweries."
-            #       sleep(2)
-            #       puts "Thank brew very much! Have a hoppy day!"
+                  main_menu
             end
       
       end
 
-      #Create method to save bookmarked breweries
-
-
-
-      # def display_list_of_breweries
-      #       Brewery.all.each_with_index do |brewery, index|
-      #          puts "#{index+1}. #{brewery}"
-      #       end
-      # end
 
       def brewery_select
             index = gets.strip.to_i - 1
             max_limit = Brewery.all.length - 1
             until index.between?(0,max_limit)
-                  puts "Uh oh, that brew isn't an option."
+                  puts "Uh oh, that brewery isn't an option."
                   index = gets.strip.to_i - 1
             end
             brewery_instance = Brewery.all[index]
             Brewery.display_brewery_details(brewery_instance)
       end
-
-      # def  display_brewery_details(brewery)
-      #       sleep(2)
-      #       puts "\n"
-      #       puts "\nName: " + brewery.name
-      #       puts "\nCity: " + brewery.city
-      #       puts "\nType: " + brewery.brewery_type
-      #       puts "\nWebsite URL: " + brewery.website_url
-         
-      #   end
-
 end
 
 
